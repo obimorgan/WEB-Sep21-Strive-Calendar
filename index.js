63211fb227aa44e79d11e95267d41cca3c85e504
Daniele Banovaz <daniele.banovaz@gmail.com>
2021-09-30 11:00:20 +0200

    We can now select days by clicking on them

--- index.js
+++ index.js
@@ -13,11 +13,40 @@ const displayMonth = function() {
         newDayNode.classList.add("day") // <div class="day"></div>
         newDayNode.innerText = dayNumber // <div class="day">1</div>
 
+        // Set the "selectDay" function as a LISTENER to the CLICK event
+        newDayNode.onclick = selectDay
+        // (the other alternative would be)
+        //newDayNode.addEventListener("click", selectDay)
+
         // ...and we attach it as the last child of the month container
         monthContainerNode.appendChild(newDayNode)
     }
 }
 
+const selectDay = function(eventData) {
+
+    // If any other day is currently selected, DESELECT that
+    const currentlySelectedDay = getSelectedDay()
+    if (currentlySelectedDay != null) {
+        currentlySelectedDay.classList.remove("selected")
+    }
+
+    // Find clicked element
+    const clickedDayNode = eventData.target // This will give us the HTML node that has been clicked
+
+    // Make it "selected" somehow
+    clickedDayNode.classList.add("selected") // then we apply the 'selected' class to it
+}
+
+const getSelectedDay = function() {
+    // we find the first element in the page that has the "selected" class assigned
+    return document.querySelector(".selected")
+}
+
 window.onload = function() {
+
+    // ALWAYS put all your instructions inside of FUNCTIONS!
+    // ...and if you need to run them as soon as the page loads,
+    // use window.onload
     displayMonth()
 }
